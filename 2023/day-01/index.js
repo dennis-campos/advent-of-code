e6d3712387ce689b015cb8324f2d60d4522c4a63
Dennis campos <97467610+dennis-campos@users.noreply.github.com>
2023-12-04 11:28:58 -0500

    day1 2023

---
+++ 2023/day-01/index.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const calibration = fs.readFileSync('./input.txt', 'utf-8').split('\n')
+
+// Part one
+function getTotalDigits(calibration) {
+    let total = 0; 
+
+    calibration.forEach((line) => {
+        let firstDigit;
+        let lastDigit; 
+    
+        for (let i = 0; i < line.length; i++) {
+            if (!isNaN(parseInt(line[i]))) {
+                firstDigit = line[i]
+                break;
+            }
+        }
+    
+        for (let i = line.length - 1; i >= 0; i--) {
+            if (!isNaN(parseInt(line[i]))) {
+                lastDigit = line[i]
+                break;
+            }
+        }
+    
+        let value = parseInt(firstDigit + lastDigit) 
+        total += value;
+    })
+
+
+    return total;
+}
+
+console.log('Answer: ', getTotalDigits(calibration))
+
+// part two
+function convertNumber(string) {
+    const digits = string
+        .replace(/one/g, 'one1one')
+        .replace(/two/g, 'two2two')
+        .replace(/three/g, 'three3three')
+        .replace(/four/g, 'four4four')
+        .replace(/five/g, 'five5five')
+        .replace(/six/g, 'six6six')
+        .replace(/seven/g, 'seven7seven')
+        .replace(/eight/g, 'eight8eight')
+        .replace(/nine/g, 'nine9nine')
+        .replace(/\D/g, '')
+
+    return Number(digits[0] + digits.slice(-1))
+}
+
+function partTwo(nums) {
+    let sum = 0
+
+    nums.forEach((num) => {
+        sum += convertNumber(num)
+    })
+
+    return sum;
+}
+
+console.log('Answer: ', partTwo(calibration))
